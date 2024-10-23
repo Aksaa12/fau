b2cdaf0b62f14eb10a692c20e021187f5e7fdce7
Aksaa12 <gesersendi@gmail.com>
2024-10-24 05:49:41 +0700

    Update core1.js

--- walrush-testnet-main/core1.js
+++ walrush-testnet-main/core1.js
@@ -1,16 +1,10 @@
 import { decodeSuiPrivateKey } from "@mysten/sui/cryptography";
-import {
-  FaucetRateLimitError,
-  getFaucetHost,
-  requestSuiFromFaucetV0,
-} from "@mysten/sui/faucet";
 import { Ed25519Keypair } from "@mysten/sui/keypairs/ed25519";
 import { Transaction } from "@mysten/sui/transactions";
 import { getFullnodeUrl, SuiClient } from "@mysten/sui/client";
 import { MIST_PER_SUI } from "@mysten/sui/utils";
 import fs from 'fs';
-import { Config } from "./app/config/config.js"; // Sesuaikan jalur sesuai kebutuhan
-import { COINENUM } from "./app/src/core/coin/coin_enum.js"; // Jalur ini sesuai dengan struktur direktori Anda
+import { COINENUM } from "./app/src/core/coin/coin_enum.js"; // Sesuaikan jalur sesuai kebutuhan
 import logger from "./app/src/utils/logger.js"; // Pastikan jalur ini sesuai
 import { Helper } from "./app/src/utils/helper.js"; // Menggunakan kurung kurawal
 
@@ -22,7 +16,6 @@ export default class Core {
     this.loadPrivateKey();
     console.log("Private key loaded:", this.acc); // Log untuk memastikan kunci privat dimuat
 
-    this.txCount = 0;
     this.client = new SuiClient({ url: getFullnodeUrl("testnet") });
     this.walrusAddress = "0x9f992cc2430a1f442ca7a5ca7638169f5d5c00e0ebc3977a65e9ac6e497fe5ef";
     this.walrusPoolObjectId = "0x37c0e4d7b36a2f64d51bba262a1791f844cfd88f31379f1b7c04244061d43914";
@@ -49,34 +42,6 @@ export default class Core {
     }
   }
 
-  async getBalance(showLogs = false) {
-    try {
-      if (showLogs) {
-        await Helper.delay(500, this.acc, "Getting Account Balance...", this);
-      }
-      this.balance = await this.client.getAllBalances({
-        owner: this.address,
-      });
-      this.balance = this.balance.map((balance) => {
-        balance.totalBalance = parseFloat(
-          (Number(balance.totalBalance) / Number(MIST_PER_SUI)).toFixed(2)
-        );
-        return balance;
-      });
-      if (showLogs) {
-        await Helper.delay(
-          1000,
-          this.acc,
-          "Successfully Get Account Balance",
-          this
-        );
-      }
-    } catch (error) {
-      console.error("Error getting balance:", error);
-      throw error;
-    }
-  }
-
   async stakeWalToOperator() {
     console.log("Memulai staking..."); // Log untuk memulai staking
     try {
@@ -85,6 +50,7 @@ export default class Core {
         owner: this.address,
         coinType: COINENUM.WAL,
       });
+
       console.log("Coins found:", coins); // Log untuk menunjukkan koin yang ditemukan
       if (coins.data.length < 1) {
         throw new Error("No WAL coins available to stake.");
@@ -141,7 +107,6 @@ export default class Core {
         `Tx Executed : ${result.digest}`,
         this
       );
-      await this.getBalance();
     } catch (error) {
       console.error("Error executing transaction:", error);
       throw error;
